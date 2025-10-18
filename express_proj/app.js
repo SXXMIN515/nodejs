e69d78abac7d44edcbcca8abd8422dfb9b96803a
sxxmin515 <ksb327d2@gmail.com>
2025-10-18 22:19:16 +0900

    251018

--- express_proj/app.js
+++ express_proj/app.js
@@ -1,8 +1,12 @@
+// app.js
+// 사용하는 모듈.
 const express = require("express");
 const fs = require("fs");
 const cookieSession = require("cookie-session");
 const multer = require("multer");
+const { userCheck } = require("./user.js");
 
+// 라우터 모듈.
 const customerRouter = require("./routes/customers");
 const productRouter = require("./routes/products");
 const boardRouter = require("./routes/boards");
@@ -10,6 +14,7 @@ const boardRouter = require("./routes/boards");
 // 서버인스턴스.
 const app = express();
 
+// 서버 설정.
 // body-parser 대신 express 내장함수 사용.
 // parsing application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: false })); // user=1234&name=hong
@@ -32,15 +37,19 @@ app.use(
 const storage = multer.diskStorage({
   // 업로드된 파일이 저장될 위치 설정.
   destination: function (req, file, cb) {
-    cb(null, "uploads/");
+    const uploadDir = "upload/images";
+    if (!fs.existsSync(uploadDir)) {
+      // 폴더가 없으면 동기적으로 생성합니다.
+      fs.mkdirSync(uploadDir, { recursive: true });
+    }
+    cb(null, uploadDir);
   },
   // 업로드된 파일의 이름 설정.
   filename: function (req, file, cb) {
-    // 한글 파일명 깨짐 방지
-    const originalName = Buffer.from(file.originalname, "latin1").toString(
-      "utf8"
-    );
-    cb(null, Date.now() + "-" + originalName);
+    // 파일명에 포함된 한글을 Buffer로 처리하여 안전한 파일명 생성
+    const originalName = //
+      Buffer.from(file.originalname, "latin1").toString("utf8"); // 한글처리.
+    cb(null, Date.now().valueOf() + originalName); // 2025-08-20-시간+홍길동.jpg
   },
 });
 // multer 객체 생성.
@@ -54,7 +63,6 @@ app.use("/products", productRouter); // '/', '/add'
 
 // 라우팅 정보 : '/' -> 'page정보', '/list' -> '글목록정보'
 // get/post/put/delete 요청정보 처리결과 출력.
-
 app.get("/", (req, res) => {
   fs.readFile("./root.html", "utf8", (err, data) => {
     if (err) {
@@ -71,101 +79,45 @@ app.post("/", (req, res) => {
 });
 
 // 파일업로드 테스트.
-app.post("/upload", upload.single("profile"), (req, res) => {
-  // 'profile'은 form에서 업로드하는 파일의 name속성값.
-  console.log(req.file); //업로드된 파일 정보.
-  res.send("파일 업로드 완료!");
-});
-
-// 숙제: 여러파일 업로드 처리.
-
-// cookie-session 테스트.
-app.get("/", (req, res) => {
-  fs.readFile("./root.html", "utf8", (err, data) => {
-    if (err) {
-      res.status(500).send("Error reading file");
-      return;
-    }
-    res.send(data);
-  });
-});
-
-fs.readFile("./user_info.txt", "utf-8", (err, data) => {
-  if (err) {
-    res.status(500).send("Error reading file");
-    return;
-  }
-  let string = data
-    .split("\r\n")
-    .filter((arr) => {
-      return arr.trim(" ");
-    })
-    .map((line) => {
-      const [id, pw, name] = line.split(",");
-      return { id, pw, name };
-    });
-  console.log(string);
-
-  app.get("/login", (req, res) => {
-    // if (!req.session.views) {
-    //   req.session.views = 1;
-    // } else {
-    //   req.session.views++;
-    // }
-    // res.send(`현재 ${req.session.views}번째 방문입니다.
-    //   <br><a href="/logout">로그아웃</a>`);
-    // res.send(`<!DOCTYPE html>
-    //           <html lang="en">
-    //             <head>
-    //               <meta charset="UTF-8" />
-    //               <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    //               <title>Document</title>
-    //             </head>
-    //             <body>
-    //               <p>학번 : ${sno}</p>
-    //               <p>이름 : ${sname}</p>
-    //               <p>합격여부 : ${
-    //                 score >= 60 ? `합격(${score})` : `불합격(${score})`
-    //               }</p>
-    //             </body>
-    //           </html>`);
-  });
-});
-
-// app.post("/login", (req, res) => {
-//   if (!req.session.views) {
-//     req.session.views = 1;
-//   } else {
-//     req.session.views++;
-//   }
-
-//   res.send(`현재 ${req.session.views}번째 방문입니다.
-//     <br><a href="/logout">로그아웃</a>`);
+// 파일 하나 업로드
+// app.post("/upload", upload.single("profile"), (req, res) => {
+//   // 'profile'은 form에서 업로드하는 파일의 name속성값.
+//   console.log(req.file); //업로드된 파일 정보.
+//   res.send("파일 업로드 완료!");
 // });
 
-app.post("/test", (req, res) => {
-  console.log(req.body);
-  let pass;
-  if (req.body.score >= 60) {
-    pass = `합격(${req.body.score})`;
-  } else {
-    pass = `불합격(${req.body.score})`;
+// 숙제: 여러파일 업로드 처리.
+app.post("/upload", upload.array("profile", 10), (req, res) => {
+  // 'profile'은 form에서 업로드하는 파일의 name속성값.
+  console.log(req.files); // 여러 파일 정보 (배열)
+  res.send("여러 파일 업로드 완료!");
+});
+
+// login 처리. get/post 비교.
+app.get("/login/:username/:password", async (req, res) => {
+  const { username, password } = req.params;
+  try {
+    const result = await userCheck(username, password); // 비동기처리.
+    res.send(result);
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: "서버 내부 오류가 발생했습니다.",
+    });
   }
-  res.send(
-    `<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-    </head>
-    <body>
-    <p>학번 : ${req.body.sno}</p>
-    <p>이름 : ${req.body.sname}</p>
-    <p>합격여부 : ${pass}</p>
-    </body>
-    </html>`
-  );
+});
+
+app.post("/login", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const result = await userCheck(username, password); // 비동기처리.
+    res.send(result);
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: "서버 내부 오류가 발생했습니다.",
+    });
+  }
 });
 
 app.get("/logout", (req, res) => {
